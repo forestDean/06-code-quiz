2261e5380c08be99f54612e00399c647341f9936
forestDean <113769986+forestDean@users.noreply.github.com>
2023-04-22 20:32:29 +0100

    answer button functions

---
+++ assets/js/scores.js
@@ -0,0 +1,8 @@
+
+
+    // Append ordered list 
+    // questionsCh.appendChild(listCh);
+    // listCh.appendChild(li1);
+    // listCh.appendChild(li2);
+    // listCh.appendChild(li3);
+    // listCh.appendChild(li4);
