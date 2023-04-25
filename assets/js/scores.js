9163b232eee8d2f3e0fa46fec86116081132db6f
forestDean <113769986+forestDean@users.noreply.github.com>
2023-04-25 11:49:15 +0100

    submit score function addScore()

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,14 +1,31 @@
-// // Create ordered list element
-// var listCh = document.createElement("ol");
-// // Create ordered list items
-// var li1 = document.createElement("li");
-// var li2 = document.createElement("li");
-// var li3 = document.createElement("li");
-// var li4 = document.createElement("li");
+var clearButton = document.querySelector("#clear");
+
+// Create ordered list element
+//var listHs = document.createElement("ol");
+// Create ordered list items
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
 
     // Append ordered list 
-    // questionsCh.appendChild(listCh);
-    // listCh.appendChild(li1);
-    // listCh.appendChild(li2);
-    // listCh.appendChild(li3);
-    // listCh.appendChild(li4);
+    var highScores = document.getElementById("highscores")
+    //highScores.appendChild(listHs);
+    highScores.appendChild(li1);
+    li1.textContent = "test";
+    highScores.appendChild(li2);
+    li2.textContent = "test";
+    highScores.appendChild(li3);
+    li3.textContent = "test";
+    highScores.appendChild(li4);
+    li4.textContent = "test";
+    console.log('highScores');
+
+    //iterate through highScoreArray
+    //limit array to 50 results
+
+    //addeventListener to Clear Button
+    clearButton.addEventListener("click", function(){
+        // save empty highScoreArray 
+        console.log('CLEARED');
+    });
